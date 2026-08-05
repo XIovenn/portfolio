505ac322c4b5a8e8efa32589b24ccd8534599e05
XIovenn <xi.oven9@gmail.com>
2026-08-05 17:29:22 +0800

    增加了react，设置了一个taskboard看板

---
+++ taskboard-front/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App(){
+  return(
+    <div className='app-container'>
+      <h1>🚀TaskBoard</h1>
+      <p>This is my first React project.</p>
+    </div>
+  )
+}
+
+export default App
